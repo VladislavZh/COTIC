88501ec8deac770d2f9ff9e12e3d43c2934da3ff
VladislavZh <vlad18_97@mail.ru>
2022-08-03 23:21:33 +0500

    Update extra_head_model.py

--- src/models/extra_head_model.py
+++ src/models/extra_head_model.py
@@ -62,7 +62,6 @@ class ExtrHeadEventModule(LightningModule):
         if stage == 'test':
             loss1, loss2 = self.test_metrics.compute_loss_and_add_values(self, batch, outputs)
 
-        print(loss1)
         return loss1, loss2
 
     def training_step(self, batch: Any, batch_idx: int, optimizer_idx):
