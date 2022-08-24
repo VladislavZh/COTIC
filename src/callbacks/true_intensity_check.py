d32fc4ff5b6812a7bda0a1882879aa774d652fac
VladislavZh <vlad18_97@mail.ru>
2022-08-24 13:29:40 +0300

    Update true_intensity_check.py

--- src/callbacks/true_intensity_check.py
+++ src/callbacks/true_intensity_check.py
@@ -27,6 +27,7 @@ class PrintingCallback(Callback):
             self.true_model = None
         
         self.sim_size = sim_size
+        self.idx = 0
         
         self.experiment = Experiment(
             api_key="eTfGsxdNWCvov8CuHEC0ug9ko",
@@ -64,14 +65,14 @@ class PrintingCallback(Callback):
     def on_validation_epoch_end(self, trainer, pl_module):
         times, events = trainer.datamodule.data_val[0]
         non_pad_mask = events.ne(0)
-        times = times[non_pad_mask]
-        events = events[non_pad_mask]
+        times = times[non_pad_mask].clone()
+        events = events[non_pad_mask].clone()
         
         times = times.unsqueeze(0)
         events = events.unsqueeze(0)
         
         full_times = self.__add_sim_times(torch.concat([torch.zeros(1,1),times], dim = 1), self.sim_size)
-        print(full_times.shape)
+        
         if self.true_model:
             tracked_intensity_true = []
             for t in full_times[0]:
@@ -84,13 +85,16 @@ class PrintingCallback(Callback):
 
         enc_output = pl_module.net(times, events)
         event_time = torch.concat([torch.zeros(times.shape[0],1).to(times.device), times], dim = 1)
-        non_pad_mask = torch.concat([torch.ones(event_time.shape[0],1).to(event_time.device),  events.ne(0).long()], dim = 1).long()
-        tracked_intensity = torch.sum(pl_module.net.final(full_times, event_time, enc_output, non_pad_mask, self.sim_size), dim=-1).detach().cpu().numpy() # shape = (bs, (num_samples + 1) * L + 1, num_types)
+        non_pad_mask = torch.concat([torch.ones(event_time.shape[0],1).to(event_time.device),  events.ne(0).long()], dim = 1).bool()
+        
+        tracked_intensity = torch.sum(pl_module.net.final(full_times, event_time, enc_output, non_pad_mask, self.sim_size), dim=-1).detach().cpu().numpy()[0] # shape = (bs, (num_samples + 1) * L + 1, num_types)
         
         fig = plt.figure(figsize=(16,9), dpi=300)
         if self.true_model:
-            plt.plot(full_times.detach().cpu().numpy(), tracked_intensity_true, label = "True model")
-        plt.plot(full_times.detach().cpu().numpy(), tracked_intensity, label = "Predicted model")
+            plt.plot(full_times.detach().cpu().numpy()[0], tracked_intensity_true, label = "True model")
+        plt.plot(full_times.detach().cpu().numpy()[0], tracked_intensity, label = "Predicted model")
         plt.legend(loc="upper right", bbox_to_anchor=(1,1))
+        plt.savefig('imgs/{}.pdf'.format(self.idx))
+        self.idx += 1
         self.experiment.log_figure(figure=plt)
         
