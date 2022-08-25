76690ec610d24221b552e4dddd74961b09b9913b
VladislavZh <vlad18_97@mail.ru>
2022-08-25 23:04:02 +0300

    test

--- src/models/components/cont_cnn/ccnn.py
+++ src/models/components/cont_cnn/ccnn.py
@@ -70,4 +70,4 @@ class CCNN(nn.Module):
         out = self.final_list[0](times, true_times, true_features, non_pad_mask, sim_size)
         for layer in self.final_list[1:]:
             out = layer(out)
-        return out/100
+        return out
