3f7d55f2349613292c245abd744b9b7ea4aab7e7
VladislavZh <vlad18_97@mail.ru>
2022-11-25 05:28:05 +0300

    Update loss.py

--- src/utils/metrics/loss.py
+++ src/utils/metrics/loss.py
@@ -8,7 +8,7 @@ class LogCoshLoss(torch.nn.Module):
     @staticmethod
     def __log_cosh(x: torch.Tensor) -> torch.Tensor:
         out = x + torch.nn.functional.softplus(-2. * x) - math.log(2.0)
-        out[x > 20] = x[x > 20]
+        out[torch.abs(x) > 20] = torch.abs(x[torch.abs(x) > 20])
         return out
 
     def forward(
