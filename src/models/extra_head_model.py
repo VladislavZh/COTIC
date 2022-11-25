f3f5097eb85af9681a76ff489fa9f06bc7f019fc
VladislavZh <vlad18_97@mail.ru>
2022-11-25 16:23:17 +0300

    Update extra_head_model.py

--- src/models/extra_head_model.py
+++ src/models/extra_head_model.py
@@ -132,8 +132,8 @@ class ExtrHeadEventModule(LightningModule):
 
     def configure_optimizers(self):
         optimizer = get_optimizer(self.hparams.optimizers[0]['name'], torch.nn.ModuleList([self.net, self.head]).parameters(), self.hparams.optimizers[0]['params'])
-        milestones = [10,20]
-        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones, gamma=0.1, verbose=True)
+        milestones = [10,20,30,40,50,60]
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones, gamma=0.5, verbose=True)
         return [optimizer], [scheduler]
         # optimizer1 = get_optimizer(self.hparams.optimizers[0]['name'], self.net.parameters(), self.hparams.optimizers[0]['params'])
         # optimizer2 = get_optimizer(self.hparams.optimizers[1]['name'], self.head.parameters(), self.hparams.optimizers[1]['params'])
