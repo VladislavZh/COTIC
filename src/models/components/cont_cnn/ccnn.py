82031040ba8ae1613ecb3972444904ab231ab216
VladislavZh <vlad18_97@mail.ru>
2022-11-26 14:11:39 +0300

    Update ccnn.py

--- src/models/components/cont_cnn/ccnn.py
+++ src/models/components/cont_cnn/ccnn.py
@@ -81,7 +81,7 @@ class CCNN(nn.Module):
 
         for i, conv in enumerate(self.convs):
             enc_output = torch.nn.functional.leaky_relu(conv(event_times, enc_output, non_pad_mask),0.1)
-            enc_output = self.dropouts[i](enc_output)
+            #enc_output = self.dropouts[i](enc_output)
             final_enc_output += self.convs_skip_connections[i](enc_output.transpose(1,2))
 
 
