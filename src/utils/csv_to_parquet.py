9f9956d4e069c807b5c60ec4e5a33a006725d127
Seva Grabar <grabarseva@gmail.com>
2022-11-30 10:39:14 +0000

    argparser

--- src/utils/csv_to_parquet.py
+++ src/utils/csv_to_parquet.py
@@ -1,42 +1,46 @@
+import argparse
 import glob
 import os
 import re
 
 import pandas as pd
 import pyarrow as pa
+import pyarrow.parquet as pq
 import tqdm
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--dataset-name", default="ATM")
+args = vars(parser.parse_args())
 data_folder = "data"
-dataset = "ATM"
+dataset = args["dataset_name"]
 time_col = "time"
-event_col = "event"
+event_col = "category_2"
 
 csvs = glob.glob(os.path.join(data_folder, dataset, "*.csv"))
 # filter list by files containing digits
 csvs = [c for c in csvs if bool(re.search("\d", c))]
 
-user_id = csvs[0].split("/")[-1]
-user_id = user_id.split(".")[0]
-total_df = pd.read_csv(csvs[0])
-total_df = total_df[[time_col, event_col]]
-total_df["user_id"] = user_id
+seq_id = csvs[0].split("/")[-1]
+seq_id = seq_id.split(".")[0]
+total_df = pd.read_csv(csvs[0], usecols=[time_col,event_col])
+total_df["sequence_id"] = seq_id
 
 for i in tqdm.tqdm(range(1, len(csvs)), total=len(csvs) - 1, miniters=1):
-    user_id = csvs[i].split("/")[-1]
-    user_id = user_id.split(".")[0]
-    curr_df = pd.read_csv(csvs[i])
-    curr_df = total_df[[time_col, event_col]]
-    curr_df["user_id"] = user_id
+    seq_id = csvs[i].split("/")[-1]
+    seq_id = seq_id.split(".")[0]
+    curr_df = pd.read_csv(csvs[0], usecols=[time_col,event_col])
+    curr_df["sequence_id"] = seq_id
     total_df = pd.concat([total_df, curr_df])
 
 
 # compressing datatypes
-# total_df[time_col] = total_df[time_col].astype("float16")
-# if total_df.dtypes[event_col] == "int64":
-#     total_df[event_col] = total_df[event_col].astype("int8")
-total_df.reset_index(inplace=True)
+total_df[time_col] = total_df[time_col].astype("float32")
+if total_df.dtypes[event_col] == "int64":
+    total_df[event_col] = total_df[event_col].astype("int8")
+total_df.reset_index(inplace=True, drop=True)
 print(total_df.info())
+print(total_df.sample(n=5))
 
 # saving to parquet
 pq_table = pa.Table.from_pandas(total_df)
-pa.parquet(pq_table, os.path.join(data_folder, dataset + ".parquet"))
+pq.write_table(pq_table, os.path.join(data_folder, dataset + ".parquet"))
