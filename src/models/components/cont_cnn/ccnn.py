cfe441563e072cdb84d7e0a5cb00066e3d3647fe
VladislavZh <vlad18_97@mail.ru>
2022-11-30 16:51:54 +0300

    best reproduce

--- src/models/components/cont_cnn/ccnn.py
+++ src/models/components/cont_cnn/ccnn.py
@@ -29,10 +29,10 @@ class CCNN(nn.Module):
 
         self.convs = nn.ModuleList([ContConv1d(kernel.recreate(self.in_channels[i]), kernel_size, self.in_channels[i], nb_filters, self.dilation_factors[i], include_zero_lag[i]) for i in range(nb_layers)])
 
-        self.final_list = nn.ModuleList([ContConv1dSim(LinearKernel(nb_filters, nb_filters), 1, nb_filters, nb_filters), nn.LeakyReLU(0.1), nn.Linear(nb_filters, num_types), nn.Softplus(100)])
+        self.final_list = nn.ModuleList([ContConv1dSim(kernel.recreate(self.nb_filters), 1, nb_filters, nb_filters), nn.LeakyReLU(0.1), nn.Linear(nb_filters, num_types), nn.Softplus(100)])
 
         self.head = head
-        
+
     def __add_bos(self, event_times, event_types, lengths):
         bs, L = event_times.shape
         event_times = torch.concat([torch.zeros(bs, 1).to(event_times.device), event_times], dim = 1)
