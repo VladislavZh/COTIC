0c267741051fefd8e768e9326fa85c495f8c9c69
Seva Grabar <grabarseva@gmail.com>
2022-12-01 01:21:45 +0000

    change with for loop

--- src/utils/csv_to_parquet.py
+++ src/utils/csv_to_parquet.py
@@ -14,7 +14,7 @@ args = vars(parser.parse_args())
 data_folder = "data"
 dataset = args["dataset_name"]
 time_col = "time"
-event_col = "category_2"
+event_col = "event"
 
 csvs = glob.glob(os.path.join(data_folder, dataset, "*.csv"))
 # filter list by files containing digits
@@ -25,18 +25,18 @@ seq_id = seq_id.split(".")[0]
 total_df = pd.read_csv(csvs[0], usecols=[time_col,event_col])
 total_df["sequence_id"] = seq_id
 
-for i in tqdm.tqdm(range(1, len(csvs)), total=len(csvs) - 1, miniters=1):
+for i in tqdm.tqdm(range(1, len(csvs)), total=len(csvs), miniters=1):
     seq_id = csvs[i].split("/")[-1]
     seq_id = seq_id.split(".")[0]
-    curr_df = pd.read_csv(csvs[0], usecols=[time_col,event_col])
+    curr_df = pd.read_csv(csvs[i], usecols=[time_col,event_col])
     curr_df["sequence_id"] = seq_id
     total_df = pd.concat([total_df, curr_df])
 
-
+print(total_df.event.unique())
 # compressing datatypes
 total_df[time_col] = total_df[time_col].astype("float32")
 if total_df.dtypes[event_col] == "int64":
-    total_df[event_col] = total_df[event_col].astype("int8")
+    total_df[event_col] = total_df[event_col].astype("int16")
 total_df.reset_index(inplace=True, drop=True)
 print(total_df.info())
 print(total_df.sample(n=5))
