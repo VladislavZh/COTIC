72b5723a8c9082d231815ecaccf15678a154adda
Vsevolod Grabar <v.grabar@skoltech.ru>
2024-01-31 20:46:47 +0300

    datamodule

--- src/datamodules/base_event_sequence_datamodule.py
+++ src/datamodules/base_event_sequence_datamodule.py
@@ -1,122 +1,125 @@
-from typing import Optional
+from typing import Optional, Tuple
 import os
 
+import torch
 from pytorch_lightning import LightningDataModule
-from torch.utils.data import DataLoader
-from .components.base_dset import EventDataset
+from torch.utils.data import DataLoader, Dataset, random_split
+from .components.base_dset import EventData
 
-from src.utils.data_utils import load_time_series_data
+from src.utils.data_utils import load_data_simple, load_data
+
 
 
 class EventDataModule(LightningDataModule):
     """
-    General event sequence data module.
-    This LightningDataModule handles loading and preparing event sequence datasets
-    for training, validation, and testing in PyTorch Lightning workflows.
+    General event sequence datamodule
     """
 
     def __init__(
-            self,
-            num_event_types: int,
-            data_dir: str = "data/",
-            batch_size_train: int = 20,
-            batch_size_val_test: int = 1,
-            dataset_size_train: Optional[int] = None,
-            dataset_size_val: Optional[int] = None,
-            dataset_size_test: Optional[int] = None,
-            num_workers: int = 0,
-            pin_memory: bool = False,
+        self,
+        data_dir: str = "data/",
+        unix_time: bool = False,
+        train_val_test_split: Tuple[float, float, float] = (0.8, 0.1, 0.1),
+        batch_size: int = 64,
+        dataset_size: Optional[int] = None,
+        dataset_size_train: Optional[int] = None,
+        dataset_size_val: Optional[int] = None,
+        dataset_size_test: Optional[int] = None,
+        max_len: Optional[int] = None,
+        num_event_types: Optional[int] = None,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+        random_seed: int = 42,
+        preprocess_type: str = "default",
     ):
-        """
-        Initializes the EventDataModule.
-
-        Args:
-            num_event_types (int): Number of unique event types.
-            data_dir (str): Path to the directory containing the dataset.
-            batch_size_train (int): Batch size for training data loading.
-            batch_size_val_test (int): Batch size for validation and test data loading.
-            dataset_size_train (Optional[int]): Size of the training dataset.
-            dataset_size_val (Optional[int]): Size of the validation dataset.
-            dataset_size_test (Optional[int]): Size of the test dataset.
-            num_workers (int): Number of workers for data loading.
-            pin_memory (bool): Flag to enable memory pinning.
-        """
         super().__init__()
 
-        self.num_event_types = num_event_types
-        self.data_dir = data_dir
-        self.batch_size_train = batch_size_train
-        self.batch_size_val_test = batch_size_val_test
-        self.dataset_size_train = dataset_size_train
-        self.dataset_size_val = dataset_size_val
-        self.dataset_size_test = dataset_size_test
-        self.num_workers = num_workers
-        self.pin_memory = pin_memory
+        self.save_hyperparameters(logger=False)
 
-        self.data_train = None
-        self.data_val = None
-        self.data_test = None
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
 
-    def load_event_data(self, data_path: str, dataset_size: Optional[int]) -> EventDataset:
-        """
-        Loads event data from a specified path and creates an EventData instance.
-
-        Args:
-            data_path (str): Path to the dataset.
-            dataset_size (Optional[int]): Size of the dataset.
-
-        Returns:
-            EventData: An instance of EventData with loaded data.
-        """
-        times, events = load_time_series_data(data_path, dataset_size)
-        return EventDataset(times, events, self.num_event_types)
+    def prepare_data(self):
+        pass
 
     def setup(self, stage: Optional[str] = None):
-        """
-        Sets up the datasets for training, validation, and testing.
-
-        Args:
-            stage (Optional[str]): Stage of setup.
-        """
         if not self.data_train and not self.data_val and not self.data_test:
-            self.data_train = self.load_event_data(
-                os.path.join(self.data_dir, "train"), self.dataset_size_train
-            )
-            self.data_val = self.load_event_data(
-                os.path.join(self.data_dir, "val"), self.dataset_size_val
-            )
-            self.data_test = self.load_event_data(
-                os.path.join(self.data_dir, "test"), self.dataset_size_test
+            if "preprocess_type" in self.hparams.keys():
+                times, events = load_data(
+                    self.hparams.data_dir,
+                    self.hparams.unix_time,
+                    self.hparams.dataset_size,
+                    self.hparams.max_len,
+                    self.hparams.preprocess_type,
+                )
+            else:
+                times, events = load_data(
+                    self.hparams.data_dir,
+                    self.hparams.unix_time,
+                    self.hparams.dataset_size,
+                    self.hparams.max_len,
+                )
+            dataset = EventData(times, events)
+            N = len(dataset)
+            lengths = [int(N * v) for v in self.hparams.train_val_test_split]
+            lengths[0] = N - (lengths[1] + lengths[2])
+            self.data_train, self.data_val, self.data_test = random_split(
+                dataset=dataset,
+                lengths=lengths,
+                generator=torch.Generator().manual_seed(self.hparams.random_seed),
             )
 
-    def create_dataloader(self, dataset, batch_size: int, shuffle: bool = False) -> DataLoader:
-        """
-        Creates a DataLoader instance for the given dataset with specified batch size.
-
-        Args:
-            dataset: Dataset to create DataLoader for.
-            batch_size (int): Batch size for the DataLoader.
-            shuffle (bool): Whether to shuffle the data.
-
-        Returns:
-            DataLoader: DataLoader instance for the dataset.
-        """
+    def train_dataloader(self):
         return DataLoader(
-            dataset=dataset,
-            batch_size=batch_size,
-            num_workers=self.num_workers,
-            pin_memory=self.pin_memory,
-            shuffle=shuffle,
+            dataset=self.data_train,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
         )
 
-    def train_dataloader(self) -> DataLoader:
-        """Returns a DataLoader for the training dataset."""
-        return self.create_dataloader(self.data_train, self.batch_size_train, shuffle=True)
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
 
-    def val_dataloader(self) -> DataLoader:
-        """Returns a DataLoader for the validation dataset."""
-        return self.create_dataloader(self.data_val, self.batch_size_val_test)
+    def test_dataloader(self):
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
 
-    def test_dataloader(self) -> DataLoader:
-        """Returns a DataLoader for the test dataset."""
-        return self.create_dataloader(self.data_test, self.batch_size_val_test)
+
+class EventDataModuleSplitted(EventDataModule):
+    def setup(self, stage: Optional[str] = None):
+        self.data_process = None
+        if not self.data_train and not self.data_val and not self.data_test:
+            times, events, unique_events = load_data_simple(
+                os.path.join(self.hparams.data_dir, "train"),
+                self.hparams.dataset_size_train,
+                self.hparams.max_len,
+                self.hparams.num_event_types,
+            )
+            self.data_train = EventData(times, events)
+            times, events, _ = load_data_simple(
+                os.path.join(self.hparams.data_dir, "val"),
+                self.hparams.dataset_size_val,
+                self.hparams.max_len,
+                unique_events,
+            )
+            self.data_val = EventData(times, events)
+            times, events, _ = load_data_simple(
+                os.path.join(self.hparams.data_dir, "test"),
+                self.hparams.dataset_size_test,
+                self.hparams.max_len,
+                unique_events,
+            )
+            self.data_test = EventData(times, events)
--- src/datamodules/components/base_dset.py
+++ src/datamodules/components/base_dset.py
@@ -1,88 +1,53 @@
 import torch
 from torch.utils.data import Dataset
 
+from typing import List, Tuple
 
-class EventDataset(Dataset):
+
+class EventData(Dataset):
     """
-    Event sequence dataset.
-    This dataset takes a list of sequences of event times and event types,
-    pads them, and returns event time and event type torch Tensors.
+    Base event sequence dataset
+    Takes list of sequences of event times and event types,
+    pads them and returns event_time torch Tensor and event type torch Tensor
     """
 
-    def __init__(self, event_times: list[torch.Tensor], event_types: list[torch.Tensor], num_event_types: int):
-        """
-        Initializes the EventDataset.
-
-        Args:
-        - event_times (list[torch.Tensor]): List of torch.Tensor of shape=(length,),
-                                            representing event arrival times since the start.
-        - event_types (list[torch.Tensor]): List of torch.Tensor of shape=(length,),
-                                            representing event types in {0, 1, ..., C-1}.
-        - num_event_types (int): Number of unique event types.
-        """
-        self.num_event_types = num_event_types
-        self.__event_times, self.__event_types = self.__add_bos(*self.__pad(event_times, event_types))
+    def __init__(self, times: List[torch.Tensor], events: List[torch.Tensor]):
+        self.__times, self.__events = self.__pad(times, events)
 
     @staticmethod
-    def __pad(event_times: list[torch.Tensor], event_types: list[torch.Tensor]) -> tuple[torch.Tensor, torch.Tensor]:
+    def __pad(times, events):
         """
-        Pads a list of event times and event types sequences.
+        Takes list of times and events sequences, pads them
 
-        Args:
-        - event_times (list[torch.Tensor]): List of torch.Tensor of shape=(length,),
-                                            representing event arrival times since the start.
-        - event_types (list[torch.Tensor]): List of torch.Tensor of shape=(length,),
-                                            representing event types in {0, 1, ..., C-1}.
+        args:
+            times   - list of torch.Tensor of shape = (length,), that represents event arrival time since start
+            events  - list of torch.Tensor of shape = (length,), that represents event types in {0,1,...,C-1}
 
-        Returns:
-        - tensor_event_times (torch.Tensor): Torch tensor of shape=(dataset_size, max_len),
-                                             where max_len = max({length}) + 1, padded event times.
-        - tensor_event_types (torch.Tensor): Torch tensor of shape=(dataset_size, max_len),
-                                              where max_len = max({length}) + 1,
-                                              padded event types with shifted events
-                                              {0, ..., C-1} -> {1, ..., C}, 0 as the pad value.
+        returns:
+            times   - torch.Tensor of shape (dset_size, max_len), where max_len = max({length}) + 1, padded times
+            events  - torch.Tensor of shape (dset_size, max_len), where max_len = max({length}) + 1, padded events with shifted events
+                                                                 {0,...,C-1} -> {1,...,C}, 0 as pad value
         """
-        lengths = torch.LongTensor([time_seq.size(0) for time_seq in event_times])
-        max_len = int(torch.max(lengths))
+        lengths = torch.Tensor([len(time_seq) for time_seq in times]).long()
+        max_len = torch.max(lengths)
 
-        tensor_event_times = torch.zeros(len(event_times), max_len)
-        tensor_event_types = torch.zeros(len(event_times), max_len)
+        print(f"DATAMODULE LEN {max_len}")
 
-        for i, (time_seq, event_seq) in enumerate(zip(event_times, event_types)):
-            seq_len = time_seq.size(0)
-            tensor_event_times[i, :seq_len] = time_seq
-            tensor_event_types[i, :seq_len] = event_seq + 1
+        tensor_times, tensor_events = torch.zeros(len(times), max_len), torch.zeros(
+            len(times), max_len
+        )
 
-        return tensor_event_times, tensor_event_types.long()
+        for i, l in enumerate(lengths):
+            tensor_times[i, :l] = times[i]
+            tensor_events[i, :l] = events[i] + 1
 
-    def __add_bos(self, event_times: torch.Tensor, event_types: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
-        """
-        Add beginning of sequence (BOS) tokens to event times and event types.
+        num_events = len(torch.unique(tensor_events.long()))
+        print(f"DATAMODULE EVENT {num_events}")
 
-        Args:
-        - event_times (torch.Tensor): Tensor of event times.
-        - event_types (torch.Tensor): Tensor of event types.
+        return tensor_times, tensor_events.long()
 
-        Returns:
-        - Tuple[torch.Tensor, torch.Tensor]: Tuple containing event times and event types with BOS tokens added.
-        """
-        bos_event_times = torch.cat([torch.zeros(event_times.shape[0], 1), event_times], dim=1)
-        bos_event_types = torch.cat([torch.full((event_types.shape[0], 1), self.num_event_types + 1, dtype=torch.long), event_types], dim=1)
-        return bos_event_times, bos_event_types
+    def __len__(self):
+        return len(self.__times)
 
-    def __len__(self) -> int:
-        """Returns the length of the dataset."""
-        return len(self.__event_times)
-
-    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
-        """
-        Retrieves an item from the dataset at the specified index.
-
-        Args:
-        - idx (int): Index of the item to retrieve.
-
-        Returns:
-        - Tuple[torch.Tensor, torch.Tensor]: Tuple containing event times and event types at the given index.
-        """
-        event_times, event_types = self.__event_times[idx], self.__event_types[idx]
-        return event_times, event_types
+    def __getitem__(self, idx):
+        return self.__times[idx], self.__events[idx]
