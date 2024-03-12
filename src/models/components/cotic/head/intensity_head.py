cd324e1385bf748b2550c99ae5a93a234bedeaea
VladislavZh <vlad18_97@mail.ru>
2024-03-12 12:26:06 +0300

    Update intensity_head.py

--- src/models/components/cotic/head/intensity_head.py
+++ src/models/components/cotic/head/intensity_head.py
@@ -191,10 +191,9 @@ class IntensityHeadLinear(nn.Module):
 
         continuous_sample_embeddings = self.layer(continuous_sample_embeddings)
         params = torch.abs(self.softplus_params)
-        pre_lambdas = continuous_sample_embeddings * params
-        lambdas = torch.exp(pre_lambdas) * params
-        lambdas_linear = np.e * params + params * pre_lambdas
-        lambdas[pre_lambdas > 1] = lambdas_linear[pre_lambdas > 1]
+        lambdas = torch.exp(continuous_sample_embeddings) * params
+        lambdas_linear = np.e * params * (1 + continuous_sample_embeddings)
+        lambdas[continuous_sample_embeddings > 1] = lambdas_linear[continuous_sample_embeddings > 1]
 
         return lambdas
 
